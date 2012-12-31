d84558d17715c5d015289eb07a46b323b9f7c0ad
Peter Baker <peter@finack.com>
2012-12-31 14:55:17 -0800

    Configure simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,20 @@
-require 'healing_light'
+require 'simplecov'
 require 'coveralls'
-Coveralls.wear!
+
+SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+
+SimpleCov.start do
+    add_filter 'vendor'
+    add_filter 'spec'
+
+    add_group 'Strips', 'lib/healing_light/strip'
+    add_group 'Leds', 'lib/healing_light/led'
+    add_group 'Controls', 'lib/healing_light/control'
+    add_group 'Executables', 'bin'
+end
+
+require 'healing_light'
+require 'healing_light/cli'
 
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
