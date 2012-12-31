af380c0d90ad717b906e7545f6822a09fcbd48f7
Peter Baker <peter@finack.com>
2012-12-31 11:39:39 -0800

    Pass array or params

--- lib/healing_light/control/basic.rb
+++ lib/healing_light/control/basic.rb
@@ -1,6 +1,7 @@
 module Control
   module Basic
-    def all_on(rgb)
+    def all_on(*rgb)
+      rgb = rgb.shift if rgb.first.is_a?(Array)
       @leds.each { |led| led.value(*rgb) }
       self.publish
     end
--- spec/healing_light/control/basic_spec.rb
+++ spec/healing_light/control/basic_spec.rb
@@ -9,17 +9,14 @@ describe Control::Basic do
       subject.should_receive(:publish)
     end
 
+    it "should take params of rgb colors" do
+      subject.all_on(255,255,255)
+    end
+
     it "should take an array of rgb colors" do
       subject.all_on([255,255,255])
     end
 
-  end
-
-  context "wip #all_on" do
-    it "should take params of rgb colors" do
-      pending
-      # subject.all_on(255,255,255)
-    end
   end
 
 end
